f938c827725a5bdc5439e39a98dc62cd98b79bbc
Zach Ruiz <powersbooth10@gmail.com>
2022-08-30 20:32:07 -0500

    commit tree 74676ea9c9f1250b2bdce30590220d33c5040bbc

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+   return customerName
+}
+
+function setBestCustomer() {
+     bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'jim';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'doug'
+}
